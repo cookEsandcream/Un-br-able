e7435562e9b6567f0dc8e83c87d2e82d4a41ceeb
kris-t-tsoi <kris.t.tsoi@gmail.com>
2017-03-27 22:08:22 +1300

    set basic functionality to see if password is correct or not.
    password is currently stored inside the class itself

--- app/src/main/java/nz/ac/auckland/unbrable/LoginActivity.java
+++ app/src/main/java/nz/ac/auckland/unbrable/LoginActivity.java
@@ -3,9 +3,16 @@ package nz.ac.auckland.unbrable;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.support.design.widget.Snackbar;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText pwText;
+    String password = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +21,24 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void login(View view) {
-        Intent myIntent = new Intent(LoginActivity.this, OverviewActivity.class);
-        startActivity(myIntent);
+    public void checkLogin(View view) {
+
+        pwText = (EditText)findViewById(R.id.pwText);
+
+        //If the password is correct then redirect to screen
+        if(pwText.toString().equals(password)){
+            Log.d("Tag","password is true");
+
+            //switch to overview activity
+            Intent myIntent = new Intent(LoginActivity.this, OverviewActivity.class);
+            startActivity(myIntent);
+        }
+        //Password is incorrect
+        else{
+            Log.d("Tag","password is false");
+            pwText.setText("");
+        }
+
+
     }
 }
