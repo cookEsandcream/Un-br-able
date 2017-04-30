3178e4b8019d02c5bfd92f380ee18d92205f3850
JSCooke <jaydencooke@outlook.com>
2017-04-30 14:36:48 +1200

    Randomised name of password entries database

--- app/src/main/java/nz/ac/auckland/unbrable/PasswordDbHelper.java
+++ app/src/main/java/nz/ac/auckland/unbrable/PasswordDbHelper.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class PasswordDbHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 3;
-    public static final String DATABASE_NAME = "PasswordEntries.db";
+    public static final String DATABASE_NAME = "FFtt7YC1dESJtz7acMDw.db";
 
     public PasswordDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
